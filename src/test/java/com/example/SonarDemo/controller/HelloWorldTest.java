72d65175341846b3306bb795e5012f424bc470e1
Jeevan Ghule <jeevan.ghule89@gmail.com>
2020-04-16 15:39:37 +0530

    added test

---
+++ src/test/java/com/example/SonarDemo/controller/HelloWorldTest.java
@@ -0,0 +1,16 @@
+package com.example.SonarDemo.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloWorldTest {
+
+    @Test
+    void sayHello() {
+    }
+
+    @Test
+    void notCovered() {
+    }
+}
